ea642c05c234ab89e8db0ad5331a50ffca4416d3
Kaspar Schiess <kaspar.schiess@absurd.li>
2013-08-22 10:22:30 +0200

    More specs for pair socket

--- spec/lib/nanomsg/pair_socket_spec.rb
+++ spec/lib/nanomsg/pair_socket_spec.rb
@@ -4,11 +4,13 @@ describe NanoMsg::PairSocket do
   let(:sock1) { NanoMsg::PairSocket.new }
   let(:sock2) { NanoMsg::PairSocket.new }
 
-  def self.examples_for_transport name, tbind, tconn
+  def self.examples_for_transport tbind
+    name = tbind.split('://').first
+    
     describe "transport #{name}" do
       it 'allows simple send/recv' do
         sock1.bind(tbind)
-        sock2.connect(tconn)
+        sock2.connect(tbind)
 
         sock1.send('1234').should == 4
         sock2.recv.should == '1234'
@@ -19,5 +21,7 @@ describe NanoMsg::PairSocket do
     end
   end
 
-  examples_for_transport "ipc", "ipc:///tmp/test.ipc", "ipc:///tmp/test.ipc"
+  examples_for_transport "ipc:///tmp/test.ipc"
+  examples_for_transport "tcp://127.0.0.1:5555"
+  examples_for_transport "inproc://test"
 end
