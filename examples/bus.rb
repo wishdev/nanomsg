77ef398985f0583a5b7f0e160f73d7a88f622c73
Kaspar Schiess <kaspar.schiess@absurd.li>
2013-08-26 13:25:57 +0200

    A BUS example

---
+++ examples/bus.rb
@@ -0,0 +1,32 @@
+$:.unshift File.dirname(__FILE__) + "/../lib"
+require 'nanomsg'
+include NanoMsg
+
+buses = 3.times.map { BusSocket.new }
+
+a = 'tcp://127.0.0.1:5432'
+b = 'tcp://127.0.0.1:5433'
+
+one, two, three = *buses
+
+one.bind a
+
+two.bind b
+two.connect a
+
+three.connect a 
+three.connect b
+
+sleep 0.1
+
+one.send 'A'
+p two.recv
+p three.recv
+
+two.send 'B'
+p one.recv
+p three.recv
+
+three.send 'C'
+p one.recv
+p two.recv
